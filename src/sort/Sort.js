18785578491b7cb077513815ef2c9d73ee847346
kongyufei <kongyufei5039@outlook.com>
2019-06-30 18:36:01 +0800

    添加插入、选择排序

--- src/sort/Sort.js
+++ src/sort/Sort.js
@@ -1,5 +1,5 @@
 /**
- * 冒泡、选择、插入排序
+ * 冒泡、插入、选择排序
  */
 const bubbleSort = (arr) => {
   if (arr.length <= 1) return arr
@@ -16,4 +16,38 @@ const bubbleSort = (arr) => {
   return arr
 }
 
-export { bubbleSort }
+// 插入排序
+const insertionSort = (arr) => {
+  if (arr.length <= 1) return arr
+  for (let i = 1; i < arr.length; i++) {
+    const temp = arr[i]
+    let j = i - 1
+    // 若 arr[i] 前有大于 arr[i] 的值，向后移位，腾出空间，直到 <= arr[i]
+    for (; j >= 0; j--) {
+      if (arr[j] > temp) {
+        arr[j + 1] = arr[j]
+      } else {
+        break
+      }
+    }
+    arr[j + 1] = temp
+  }
+  return arr
+}
+
+// 选择排序
+const selectionSort = (arr) => {
+  if (arr.length <= 1) return arr
+  for (let i = 0; i < arr.length; i++) {
+    let minIndex = i
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j
+      }
+    }
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
+  }
+  return arr
+}
+
+export { bubbleSort, insertionSort, selectionSort }
--- test/sort/Sort.spec.js
+++ test/sort/Sort.spec.js
@@ -1,9 +1,24 @@
 import assert from 'assert'
-import { bubbleSort } from '@/sort/Sort.js'
+import { bubbleSort, insertionSort } from '@/sort/Sort.js'
+import { selectionSort } from '../../src/sort/Sort';
 
 describe('冒泡排序测试', () => {
   const arr = [3, 23, 321, 1, 23, 34]
   it('数组排序后输出结果', () => {
     assert.equal(bubbleSort(arr), '1,3,23,23,34,321')
+  })
+})
+
+describe('插入排序测试', () => {
+  const arr = [3, 23, 321, 1, 23, 34]
+  it('数组排序后输出结果', () => {
+    assert.equal(insertionSort(arr), '1,3,23,23,34,321')
+  })
+})
+
+describe('选择排序测试', () => {
+  const arr = [3, 23, 321, 1, 23, 34]
+  it('数组排序后输出结果', () => {
+    assert.equal(selectionSort(arr), '1,3,23,23,34,321')
   })
 })

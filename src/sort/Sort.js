73f86c3ffc73673db80c39dd89d0a64b719df83d
kongyufei <kongyufei5039@outlook.com>
2019-07-08 19:23:03 +0800

    增加归并排序和快速排序

--- src/sort/Sort.js
+++ src/sort/Sort.js
@@ -1,5 +1,5 @@
 /**
- * 冒泡、插入、选择排序
+ * 冒泡、插入、选择、归并、快速排序
  */
 const bubbleSort = (arr) => {
   if (arr.length <= 1) return arr
@@ -50,4 +50,61 @@ const selectionSort = (arr) => {
   return arr
 }
 
-export { bubbleSort, insertionSort, selectionSort }
+// 归并排序
+const mergeSort = (arr) => {
+  // 当任意数组分解到只有一个是返回
+  if (arr.length <= 1) return arr
+  const middle = Math.floor(arr.length / 2) // 找到中间值
+  const left = arr.slice(0, middle) // 分割数组
+  const right = arr.slice(middle)
+
+  // 递归合并分解的数组
+  return mergeArr(mergeSort(left), mergeSort(right))
+}
+
+const mergeArr = (left, right) => {
+  let temp = [] // 临时数组，存放合并后的数组
+  let leftIndex = 0
+  let rightIndex = 0
+  // 判断两数组中元素大小，依次插入数组
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      temp.push(left[leftIndex])
+      leftIndex++
+    } else {
+      temp.push(right[rightIndex])
+      rightIndex++
+    }
+  }
+  // 合并多余的数组
+  return [...temp, ...left.slice(leftIndex), ...right.slice(rightIndex)]
+}
+
+// 快速排序
+const quickSort = (arr, left, right) => {
+  if (left < right) {
+    let pivot = right
+    // 获取分区点
+    let partitionIndex = partition(arr, pivot, left, right)
+    // 递归排序左边部分
+    quickSort(arr, left, partitionIndex - 1 < left ? left : partitionIndex - 1)
+    // 递归排序右边部分
+    quickSort(arr, partitionIndex + 1 > right ? right : partitionIndex + 1, right)
+  }
+}
+
+// 获取 pivot 交换后的 index
+const partition = (arr, pivot, left, right) => {
+  const pivotVal = arr[pivot]
+  let startIndex = left
+  for (let i = left; i < right; i++) {
+    if (arr[i] < pivotVal) {
+      [arr[startIndex], arr[i]] = [arr[i], arr[startIndex]]
+      startIndex++
+    }
+  }
+  [arr[startIndex], arr[pivot]] = [arr[pivot], arr[startIndex]]
+  return startIndex
+}
+
+export { bubbleSort, insertionSort, selectionSort, mergeSort, quickSort }
--- test/sort/Sort.spec.js
+++ test/sort/Sort.spec.js
@@ -1,6 +1,5 @@
 import assert from 'assert'
-import { bubbleSort, insertionSort } from '@/sort/Sort.js'
-import { selectionSort } from '../../src/sort/Sort';
+import { bubbleSort, insertionSort, selectionSort, mergeSort, quickSort } from '@/sort/Sort.js'
 
 describe('冒泡排序测试', () => {
   const arr = [3, 23, 321, 1, 23, 34]
@@ -20,5 +19,20 @@ describe('选择排序测试', () => {
   const arr = [3, 23, 321, 1, 23, 34]
   it('数组排序后输出结果', () => {
     assert.equal(selectionSort(arr), '1,3,23,23,34,321')
+  })
+})
+
+describe('归并排序测试', () => {
+  const arr = [3, 23, 321, 1, 23, 34]
+  it('数组排序后输出结果', () => {
+    assert.equal(mergeSort(arr), '1,3,23,23,34,321')
+  })
+})
+
+describe('快速排序测试', () => {
+  const arr = [3, 23, 321, 111, 23, 34]
+  it('数组排序后输出结果', () => {
+    quickSort(arr, 0, arr.length - 1)
+    assert.equal(arr, '3,23,23,34,111,321')
   })
 })
